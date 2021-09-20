e7101b2363ac5995966ecbad9fd9634ee4748f9b
jason.elting <jason.elting@altamiracorp.com>
2021-09-20 08:40:03 -0500

    started apigateway

--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -1,6 +1,5 @@
-const sns = require('@aws-cdk/aws-sns');
-const subs = require('@aws-cdk/aws-sns-subscriptions');
-const sqs = require('@aws-cdk/aws-sqs');
+const lambda = require('@aws-cdk/aws-lambda')
+const apigateway = require('@aws-cdk/aws-apigateway')
 const cdk = require('@aws-cdk/core');
 
 class CdkStack extends cdk.Stack {
@@ -12,13 +11,6 @@ class CdkStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
 
